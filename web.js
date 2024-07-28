d6965cea7112968e5e0fc848712a7e6f7dd48e9b
tanishamehra24 <tanishamehra24@gmail.com>
2024-07-28 21:01:14 +0530

    Update web.js
    new changes

--- web.js
+++ web.js
@@ -15,3 +15,43 @@ let fontFamily=[
     "cursive",
     "serif"
 ];
+
+//initial settings
+    //adding font names 
+fontFamily.map((value)=>{
+  let options=document.createElement("option");
+  options.value=value;
+  options.innerHTML=value;
+  fontName.appendChild(options);
+});
+//default fontName
+fontName.value="Arial";
+
+//adding font sizes
+for(let i=1;i<=7;i++){
+    let option = document.createElement("option");
+    option.value=i;
+    option.innerHTML=i;
+    fontSize.appendChild(option);
+};
+//default size for font
+fontSize.value=3;
+
+//to modify text 
+const modifyText=(command,defaultUi,value)=>{
+document.execCommand(command,defaultUi,value);
+};
+//adding event listener for option buttons
+optionButton.forEach((button)=>{
+    button.addEventListener("click",()=>{
+    modifyText(button.id,false,null);
+    });
+    });
+
+
+//adding event listeners for buttons like font color.
+advButtons.forEach((button)=>{
+button.addEventListener("change",()=>{
+modifyText(button.id,false,button.value);
+});
+});
