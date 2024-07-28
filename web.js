200ad15423c48e1b6b0c48a2978a0dc7e7c7a173
tanishamehra24 <tanishamehra24@gmail.com>
2024-07-28 20:17:42 +0530

    new changes
    some alterations in the repo

---
+++ web.js
@@ -0,0 +1,17 @@
+// text-editor
+// accessing all the elements 
+let optionButton=document.querySelectorAll(".option-button");
+let advButtons=document.querySelectorAll(".adv-option-button");
+let fontName= document.getElementById("font-name");
+let fontSize=document.getElementById("font-size");
+let textArea=document.getElementById("textbox");
+// font names
+let fontFamily=[
+    "Arial",
+    "sans-serif",
+    "Times New Roman",
+    "Verdana",
+    "Georgia",
+    "cursive",
+    "serif"
+];
