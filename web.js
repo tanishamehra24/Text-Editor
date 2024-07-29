b7c8515828c44a6e58670e347a006a0477caa993
tanishamehra24 <tanishamehra24@gmail.com>
2024-07-29 19:42:09 +0530

    further changes in the website
    changed few content of html along with css and updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 // text-editor
 // accessing all the elements 
 let optionButton=document.querySelectorAll(".option-button");
-let advButtons=document.querySelectorAll(".adv-option-button");
+let advanceButtons=document.querySelectorAll(".adv-option-button");
 let fontName= document.getElementById("font-name");
 let fontSize=document.getElementById("font-size");
 let textArea=document.getElementById("textbox");
+
 // font names
 let fontFamily=[
     "Arial",
@@ -17,13 +18,15 @@ let fontFamily=[
 ];
 
 //initial settings
-    //adding font names 
+
+//adding font names 
 fontFamily.map((value)=>{
-  let options=document.createElement("option");
-  options.value=value;
-  options.innerHTML=value;
-  fontName.appendChild(options);
+  let option=document.createElement("option");
+  option.value=value;
+  option.innerHTML=value;
+  fontName.appendChild(option);
 });
+
 //default fontName
 fontName.value="Arial";
 
@@ -38,21 +41,21 @@ for(let i=1;i<=7;i++){
 fontSize.value=3;
 
 //to modify text 
-const modifyText=(command,defaultUi,value)=>{
-document.execCommand(command,defaultUi,value);
-};
+const modifyText=(command, defaultUi, value)=>{
+document.execCommand(command, defaultUi, value);
+}; 
 
-//adding event listener for option buttons
+//adding event listener for option buttons -working
 optionButton.forEach((button)=>{
     button.addEventListener("click",()=>{
-    modifyText(button.id,false,null);
+    modifyText(button.id, false, null);
     });
     });
 
 
 //adding event listeners for buttons like font color.
-advButtons.forEach((button)=>{
-button.addEventListener("change",()=>{
-modifyText(button.id,false,button.value);
-});
+advanceButtons.forEach((button)=>{
+    button.addEventListener("change",()=>{
+        modifyText(button.id,false,button.value);
+    });
 });
