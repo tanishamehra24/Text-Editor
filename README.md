dd5a6c27f4459d5ea13184c5dd98c117866194e5
tanishamehra24 <tanishamehra24@gmail.com>
2024-07-30 12:42:56 +0530

    final changes in the project
    did some changes in the js code along with some minute changes in html and wrote the user guide .

--- README.md
+++ README.md
@@ -8,3 +8,13 @@
 
 # This website uses HTML, CSS and JavaScript.
 
+# USER GUIDE 
+1. Text Editor is a pure Javascript web app. All processing is done locally in your web browser and on your computer.
+2. Standard text editor features are supported here like change in font color,fontsize and font family along with undo and redo text options.
+3. On selecting an option, the tool will do its work accordingly,
+  Undo - Undo the last action or text edit.
+  Redo - Redo the last action or text edit.
+  Font Color - Changes the font color.
+  Font Size - will change the font size ranging from 1 to 7.
+  Font Family - will change the font family like Arial,cursive,times new roman etc.
+4. You can also select the text and change it as you desire.
--- web.js
+++ web.js
@@ -1,65 +1,75 @@
-// text-editor
+// text-editor-using javaScript
+
 // accessing all the elements 
 let optionButton=document.querySelectorAll(".option-button");
 let advanceButtons=document.querySelectorAll(".adv-option-button");
-let fontName= document.getElementById("font-name");
-let fontSize=document.getElementById("font-size");
+let fontName= document.getElementById("fontName");
+let fontSize=document.getElementById("fontSize");
 let textArea=document.getElementById("textbox");
 
-// font names
-let fontFamily=[
+// adding a list of font names with an array
+let fontList=[
     "Arial",
-    "sans-serif",
     "Times New Roman",
     "Verdana",
     "Georgia",
+    "Courier New",
     "cursive",
-    "serif"
+    "Garamond"
 ];
 
 //initial settings
-const initializer=()=>{
-//adding font names 
-fontFamily.map((value)=>{
+//setting the initializer for changing font sizes and font names
+
+const initializer =()=>{
+
+ //adding font names 
+fontList.map((value)=>{
   let option=document.createElement("option");
   option.value=value;
   option.innerHTML=value;
   fontName.appendChild(option);
 });
 
-//default fontName
-fontName.value="Arial";
-
 //adding font sizes
+//font sizes from 1 to 7 
 for(let i=1;i<=7;i++){
     let option = document.createElement("option");
     option.value=i;
     option.innerHTML=i;
     fontSize.appendChild(option);
-};
-//default size for font
+}
+
+// setting the default size for font as 3
 fontSize.value=3;
+
 };
 
 //to modify text 
+//to make changes execCommand is used 
 const modifyText=(command, defaultUi, value)=>{
-document.execCommand(command, defaultUi, value);
-}; 
+    document.execCommand(command, defaultUi, value);
+    }; 
 
-//adding event listener for option buttons -working
+//adding event listener for option buttons like undo and redo.
+
 optionButton.forEach((button)=>{
-    button.addEventListener("click",()=>{
+    button.addEventListener("click",()=>{ //on clicking the buttons the text will undo itself or redo the change.
     modifyText(button.id, false, null);
     });
     });
 
 
-//adding event listeners for buttons like font color-working
-//font size and name- not working
+//adding event listeners for buttons like font color,font Name and font Size
+
 advanceButtons.forEach((button)=>{
     button.addEventListener("change",()=>{
-        modifyText(button.id,false,button.value);
+        modifyText(button.id,false,button.value);//on selecting the required option the text will change it's color,size or font family accordingly.
+                                                  
     });
 });
 
-window.onload=initializer();
+window.onload= initializer(); //at last enabling the initializer using window.onload which will load the website accordingly.
+
+
+
